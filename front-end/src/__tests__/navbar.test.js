e965623449cb641230612bd574a3637ef0684bbe
Kevin Kaspari <KKASPARI@goremutual.ca>
2021-02-26 15:02:07 -0500

    Update navbar test to use shallow mount

--- front-end/src/__tests__/navbar.test.js
+++ front-end/src/__tests__/navbar.test.js
@@ -1,10 +1,10 @@
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import React from "react";
 import Navbar from "../components/nav-bar.js";
 
 describe("components/navbar", () => {
-  xit("Renders with required props", async () => {
-    const wrapper = mount(<Navbar />);
+  it("Renders with required props", async () => {
+    const wrapper = shallow(<Navbar />);
     expect(wrapper).toBeTruthy();
   });
 });
