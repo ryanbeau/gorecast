eee51843cc5d8e05ff16d640759c72fae044f333
Ryan Beausoleil <ryanbeausoleil@gmail.com>
2021-04-14 15:14:55 -0400

    Move AddAccount logic from component into Data

--- front-end/src/components/account-input.js
+++ front-end/src/components/account-input.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
 import { Formik } from "formik";
 import { Button, Form } from "react-bootstrap";
 import * as yup from "yup";
 import Success from "./success";
-const { queryMe } = require("../data");
+const { mutationAddAccount } = require("../data");
 
 
 const reqdFieldMsg = "This is a required field";
@@ -14,68 +13,26 @@ const schema = yup.object({
   startBalance: yup.number().required(reqdFieldMsg),
 });
 
-const getInitialMe = () => {
-  return {
-    accounts: [],
-  }
-}
-
 const AccountInput = () => {
   const [show, setShow] = useState(false);
   const showModal = () => {
     setShow(true);
   };
-
-  const [me, setMe] = useState(getInitialMe());
   const [userJWT, setUserJWT] = useState();
   const { user, getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     getAccessTokenSilently()
       .then((token) => {
-        queryMe(`Bearer ${token}`)
-          .then(result => {
-            if (result) {
-              setMe(result);
-              setUserJWT(token);
-            }
-          })
-          .catch(error => {
-            // TODO: do something with this
-          });
+        setUserJWT(token);
       });
   }, [user, getAccessTokenSilently])
 
-  const serverUrl = process.env.REACT_APP_SERVER_URL;
-
   const onSubmit = (values) => {
 
-    const body = {
-      query: `
-        mutation addAccount($memberID: Int!, $accountName: String!, $startBalance: Float) {
-          addAccount(memberID: $memberID, accountName: $accountName, startBalance: $startBalance) {
-              accountID
-              memberID
-              accountName
-              startBalance
-          }
-        }
-      `,
-      variables: {
-        memberID: me.memberID,
-        accountName: `${values.accountName}`,
-        startBalance: parseFloat(values.startBalance),
-      },
-    };
-
-    axios
-      .post(`${serverUrl}/api/graphql`, body, {
-        headers: {
-          Authorization: "Bearer " + userJWT,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
+    mutationAddAccount(`Bearer ${userJWT}`, values.accountName, parseFloat(values.startBalance))
+      .then((result) => {
+        // TODO: refresh Profile (query Me)
       })
       .catch((err) => {
         console.log(err.message);
@@ -138,31 +95,3 @@ const AccountInput = () => {
 };
 
 export default AccountInput;
-
-/*
-export const Form = ({ onSubmit }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <div className="form-group">
-        <label htmlFor="aname">Account Name</label>
-        <input className="form-control" id="aname" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="sbalance">Starting Balance</label>
-        <input
-          type=""
-          className="form-control"
-          id="sbalance"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div className="form-group">
-        <button className="form-control btn btn-primary" type="submit">
-          Submit
-        </button>
-      </div>
-    </form>
-  );
-};
-export default Form;
-*/
